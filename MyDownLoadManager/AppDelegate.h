0fd2d8bda55fc8928ce760c52af725cad63467c0
MS <ms@gg.local>
2015-07-22 16:36:28 +0800

    Initial Commit

---
+++ MyDownLoadManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDownLoadManager
+//
+//  Created by MS on 15-7-22.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
